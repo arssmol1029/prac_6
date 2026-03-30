7907001ab91360ef58b3c6b81d9d23caa8effe96
ars <arssmol1029@gmail.com>
2026-03-30 14:20:02 +0300

    20260330 sem

---
+++ 20260330/0/basepkg/core/__main__.py
@@ -0,0 +1,7 @@
+"""
+Main module for the basepkg package.
+"""
+
+from basepkg.lib import output
+
+output(__doc__)
---
+++ 20260330/0/fun_1.py
@@ -0,0 +1,1 @@
+line = "soooooooooooooooooooooooooooooooooooooooooooooooo looooooooooooooooooooooooooooooooooooooooooooooooong string"
